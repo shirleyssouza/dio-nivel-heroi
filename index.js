bab487b5161f8d816e098f383673eeca2a3b5cd9
Shirley Souza <ssouza.shirley@hotmail.com>
2024-10-15 21:48:18 -0300

    Atualização com o desafio de Ranking do Herói

--- index.js
+++ index.js
@@ -26,3 +26,30 @@ if (xp <= 1000) {
  document.getElementById("resultado").innerHTML = resultado;
 
 }
+
+function rankearPartidas() {
+    const vitorias = document.getElementById("vitorias").value;
+    const derrotas = document.getElementById("derrotas").value;
+
+    let saldoVitorias = vitorias-derrotas
+    let nivelRanking = "";
+
+    if (saldoVitorias <= 10) {
+        nivelRanking = "Ferro";
+    } else if (saldoVitorias > 10 && saldoVitorias <= 20) {
+        nivelRanking = "Bronze";
+    } else if (saldoVitorias > 20 && saldoVitorias <= 50) {
+        nivelRanking = "Prata";
+    } else if (saldoVitorias > 50 && saldoVitorias <= 80) {
+        nivelRanking = "Ouro";
+    } else if (saldoVitorias > 80 && saldoVitorias <= 90) {
+        nivelRanking = "Diamante";
+    } else if (saldoVitorias > 90 && saldoVitorias <= 100) {
+        nivelRanking = "Lendário";
+    } else if (saldoVitorias >= 101) {
+        nivelRanking = "Imortal";
+    }
+
+    const ranking = `<h2>O Herói tem saldo de ${saldoVitorias} vitórias e está no nível: ${nivelRanking}</h2>`;
+    document.getElementById("ranking").innerHTML = ranking;
+}
